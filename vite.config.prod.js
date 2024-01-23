9f5ab79600add47344e781c106a9d00abfe1d1b9
Francisco Pereira Alvarado <gammafp@gmail.com>
2024-01-23 17:19:57 +0100

    udpate phaser msg

---
+++ vite.config.prod.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+
+const phasermsg = () => {
+    return {
+        name: 'phasermsg',
+        buildStart() {
+            process.stdout.write(`Building for production...\n`);
+        },
+        buildEnd() {
+            const line = "---------------------------------------------------------";
+            const msg = `💜💜💜 Tell us about your game! - games@phaser.io 💜💜💜`;
+            process.stdout.write(`${line}\n${msg}\n${line}\n`);
+            
+            process.stdout.write(`✨ Done ✨\n`);
+        }
+    }
+}   
+
+export default defineConfig({
+    base: './',
+    logLevel: 'warning',
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    phaser: ['phaser']
+                }
+            }
+        },
+    },
+    server: {
+        port: 8080
+    },
+    plugins: [
+        phasermsg()
+    ]
+});
