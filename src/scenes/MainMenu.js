a28e68bfe8bf463c086d22782be90445c4f0e9eb
Steve Mattsen <mattsensteve@gmail.com>
2024-06-10 16:46:08 -0400

    feat: Add responsiveness to the main menu

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -9,15 +9,37 @@ export class MainMenu extends Scene
 
     create ()
     {
-        this.add.image(512, 384, 'background');
+        const canvas = this.game.canvas;
+        const vw = canvas.width * 0.01; // 1% viewer width
+        const vh = canvas.height * 0.01; // 1% viewer height
+        const vmax = vw > vh ? vw : vh;
 
-        this.add.image(512, 300, 'logo');
+        let background = this.add.image(50 * vw, 50*vh, 'background');
+        background.scaleX = 100 * vw / background.width;
+        background.scaleY =  100 * vh / background.height;
+    
+        // Show our "Phaser" title
+        const title = this.add.image(
+            50 * vw,
+            40 * vh,
+            'logo'
+        )
+        if (vmax == vw) {
+            title.scale = 15 * vh / title.height;
+        } else {
+            title.scale = 90 * vw / title.width;
+        }
 
-        this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+        this.add.text(
+            50 * vw, 
+            66.66 * vh, 
+            'Main Menu', 
+            {
+                fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+                stroke: '#000000', strokeThickness: 8,
+                align: 'center'
+            }
+        ).setOrigin(0.5);
 
         this.input.once('pointerdown', () => {
 
--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -80,6 +80,6 @@ export class Preloader extends Scene
         //  For example, you can define global animations here, so we can use them in other scenes.
 
         //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        //this.scene.start('MainMenu');
+        this.scene.start('MainMenu');
     }
 }
