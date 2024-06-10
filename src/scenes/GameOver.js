93ba77218f21ee06202ee7496b02f70605f6ccf5
Steve Mattsen <mattsensteve@gmail.com>
2024-06-10 16:51:48 -0400

    feat: Add responsiveness to the Game Over screen

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -9,15 +9,28 @@ export class GameOver extends Scene
 
     create ()
     {
+        const canvas = this.game.canvas;
+        const vw = canvas.width * 0.01; // 1% viewer width
+        const vh = canvas.height * 0.01; // 1% viewer height
+        const vmax = vw > vh ? vw : vh;
+
         this.cameras.main.setBackgroundColor(0xff0000);
 
-        this.add.image(512, 384, 'background').setAlpha(0.5);
+        let background = this.add.image(50 * vw, 50 * vh, 'background').setAlpha(0.5);
+        background.scaleX = 100 * vw / background.width;
+        background.scaleY =  100 * vh / background.height;
 
-        this.add.text(512, 384, 'Game Over', {
-            fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+
+        this.add.text(
+            50 * vw, 
+            50 * vh, 
+            'Game Over', 
+            {
+                fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
+                stroke: '#000000', strokeThickness: 8,
+                align: 'center'
+            }
+        ).setOrigin(0.5);
 
         this.input.once('pointerdown', () => {
 
